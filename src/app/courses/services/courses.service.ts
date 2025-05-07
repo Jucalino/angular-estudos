ffe17239e846645a6971541fffce18afebd6ea22
Jucalino <joseluis.bmjnet@gmail.com>
2025-05-07 10:50:14 -0300

    Chamada HTTP Get no Angular e RXJS

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http'
+import { first, take, tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json'
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'Angular', category: 'Front-End' }
-    ];
+  list(){
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      tap(courses => console.log(courses))
+    )
   }
 }
