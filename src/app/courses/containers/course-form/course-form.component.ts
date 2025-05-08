a55105eec4aa4ccb5f2fe13648c1a9d8998251ab
Jucalino <joseluis.bmjnet@gmail.com>
2025-05-08 11:47:12 -0300

    Componentes Inteligentes x Componentes de Apresentação

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { CoursesService } from '../services/courses.service';
+import { CoursesService } from '../../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
